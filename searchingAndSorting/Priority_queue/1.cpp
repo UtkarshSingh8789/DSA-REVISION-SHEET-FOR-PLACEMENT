a63b33ff72e8779feee2582390b560ec021fad11
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-05-25 11:18:08 +0530

    Find K Pairs with Smallest Sums

--- searchingAndSorting/Priority_queue/1.cpp
+++ searchingAndSorting/Priority_queue/1.cpp
@@ -1,3 +1,11 @@
+// You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+// Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+// Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+// Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+// Output: [[1,2],[1,4],[1,6]]
+// Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
