d49c95d2cb5bb4db52b51f78b3990816931c9fb6
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-07 17:54:44 +0530

    Binary Tree Maximum Path Sum

---
+++ graph and trees/binaryTree/2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int ans=-1e9;
+    int dfs(TreeNode* root){
+        if(root==NULL) return 0;
+        int left=dfs(root->left);
+        int right=dfs(root->right);
+        int temp=max(root->val,max(left,right)+root->val);
+        ans=max({ans,temp,left+right+root->val});
+        return temp;
+    }
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
