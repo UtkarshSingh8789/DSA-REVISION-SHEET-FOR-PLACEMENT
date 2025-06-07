61c4030d5cf726fae16ea8db9a76f239b0239d9d
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-07 17:44:24 +0530

    minimumSwapsToSortAnArray

--- Array/1.cpp
+++ Array/1.cpp
@@ -1,7 +1,28 @@
 // minimu swaps to sort an array;
 // using cycle detection technique;
-
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 class Solution{
     public:
