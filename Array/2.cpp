c0c0009b077c508b906f761f297eec691529bc6f
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-07 17:47:04 +0530

    Number of Matching Subsequences-vvi

--- Array/2.cpp
+++ Array/2.cpp
@@ -1,4 +1,26 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 class Solution {
 public:
