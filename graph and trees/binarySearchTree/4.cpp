1a2978d51006f07fac2c29ab75cc4c94674478c2
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-10 15:40:00 +0530

    Validate Binary Search Tree

---
+++ graph and trees/binarySearchTree/4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    long long prev=LONG_MIN;
+    bool dfs(TreeNode* root){
+        if(root==NULL) return true;
+        if(!dfs(root->left)) return false;
+        if(prev>=root->val) return false;
+        prev=root->val;
+        return dfs(root->right);
+    }
+    bool isValidBST(TreeNode* root){
+        return dfs(root);
+    }
+};
