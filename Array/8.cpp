8abf3064bc0e3f5bdefb6f2878805926a65c862a
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-12 17:54:45 +0530

    2149. Rearrange Array Elements by Sign

---
+++ Array/8.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+// can't solve in constant space;
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        int pos=0;
+        int neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos=pos+2;
+            }
+            else{
+                ans[neg]=nums[i];
+                neg=neg+2;
+            }
+        }
+        return ans;
+    }
+};
