2ebbc0956d21126d9c72da5e36b6f3cc9da0f6e1
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-06 15:46:38 +0530

    901. Online Stock Span

---
+++ Stack/5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// online stock span;
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+    }
+    int next(int price) {
+        int count=1;
+        while(!st.empty() && price>=st.top().first){
+            count=count+st.top().second;
+            st.pop();
+        }
+        st.push({price,count});
+        return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
