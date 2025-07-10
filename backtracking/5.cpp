bbe2ad0948c8aed6ec802537cd3fe8b184a51f99
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-10 12:22:53 +0530

    Sudoku Solver

---
+++ backtracking/5.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool isValid(int row,int col,char ele,vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            if(board[(row/3)*3+i/3][(col/3)*3+i%3]==ele) return false;// only this is confusiing rest easy;
+            if(board[row][i]==ele) return false;
+            if(board[i][col]==ele) return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(int ele=1;ele<=9;ele++){
+                        if(isValid(i,j,ele+'0',board)){
+                            board[i][j]=ele+'0';
+                            if(solve(board)==true){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
